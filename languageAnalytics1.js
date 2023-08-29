230cafc39ccb3f94dcd714c2f58aaa9b1a477fc4
shubh28tyagi <99463542+shubh28tyagi@users.noreply.github.com>
2023-08-29 16:45:52 +0530

    Update and rename languageAnalytics.js to languageAnalytics1.js

--- languageAnalytics1.js
+++ languageAnalytics1.js
@@ -301,3 +301,29 @@ try {
 } catch (err) {
     console.log("Error in Comscoe Ibeat", err);
 }
+---------------------------------------------------------------------------------------------------------------
+try {
+  function fireComscore() {
+    console.log("fireComscore is working");
+    const isGDPRRegion = (window.geoinfo && window.geoinfo.isGDPRRegion) || false;
+    let csucfr = "";
+    const pthname = window?.location?.pathname || "/";
+    if (window?.getCookie("ckns_policyV2") && isGDPRRegion) {
+      csucfr = "&cs_ucfr=1";
+    }
+    const scorecardResearchURL = `https://sb.scorecardresearch.com/p?c1=2&c2=6036484&c4=${pthname}&c7=${pthname}&c9=${csucfr}`;
+    fetch(scorecardResearchURL)
+      .then(response => {
+        if (response.ok) {
+          console.log("Scorecard Research URL fetched successfully");
+        } else {
+          console.log("Failed to fetch Scorecard Research URL");
+        }
+      })
+      .catch(error => {
+        console.log("Error fetching Scorecard Research URL:", error);
+      });
+  }
+} catch (err) {
+  console.log("Error fireComscore:", err);
+}
